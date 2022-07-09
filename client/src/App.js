043699f093841b337998ac71d7367f22917d0697
Archana00N <b20082@students.iitmandi.ac.in>
2022-07-09 17:08:15 +0530

    added app.js and routes

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  matchRoutes,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" exact element={<Join />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
---
+++ client/src/components/Join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Join = () => {
+  return (
+    <div>
+      <h1>haan bhai, falak, yes</h1>
+    </div>
+  )
+};
+
+export default Join;
