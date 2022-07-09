0ee0eafcb2acb8128665d076d315ab78bbc380f2
Archana00N <b20082@students.iitmandi.ac.in>
2022-07-10 00:59:13 +0530

    Updated Login.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,15 @@ import {
   matchRoutes,
   Route,
   Routes,
-} from "react-router-dom";
-import Join from "./components/Join";
+} from "react-router-dom"; // to pass props as query parameters
+import Login from "./components/Login";
 import Chat from "./components/Chat";
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" exact element={<Join />} />
-      <Route path="/chat" element={<Chat />} />
+      <Route path="/" exact element={<Login />} /> {/* Login form at home page */}
+      <Route path="/chat" element={<Chat />} /> {/* Main chat component */}
     </Routes>
   </Router>
 );
---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>haan bhai, Chalta hai, yes</h1>
+    </div>
+  )
+};
+
+export default Login;
